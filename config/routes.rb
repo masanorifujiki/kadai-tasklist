fe3257447ec9c1434a966c25647ea880d09cd1a5
masanori.fujiki <masanori_fujiki0604@yahoo.co.jp>
2019-05-10 11:48:52 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
     
-  get 'toppages/index'
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
     root to: 'toppages#index'
+    
+    get 'signup', to: 'users#new'
     
     resources :tasks
 end
